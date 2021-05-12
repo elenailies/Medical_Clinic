45b10834aeb774acc0d6e59b0a2a4d6c8d4f3ba3
elenailies <61749769+elenailies@users.noreply.github.com>
2021-05-12 22:14:44 +0300

    Update Customer.java

--- src/proiect_pao/models/Customer.java
+++ src/proiect_pao/models/Customer.java
@@ -14,6 +14,6 @@ public class Customer {
 	    
 	 @Override
 	public String toString() {
-		 return "Customer{" + "name='" + name + '\'' + '}';
+		 return "Customer{" + "name='" + name + '}';
 	 }
 }
